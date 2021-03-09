fdf69e050c6bd57a383cf53659267b31a0ddf377
Berenice Pendzialek <berenice.pendzialek@gmail.com>
2021-03-09 15:03:01 +0100

    Add logic to show the desired information on the workout card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import WorkoutCard from './components/WorkoutCard'
+import React from 'react'
+import styled from 'styled-components/macro'
+import { results } from './staticWorkoutList.json'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <WorkoutCard />
-    </div>
+    <AppLayout>
+      <h2> Select your workout:</h2>
+      {results.map(
+        ({
+          Name,
+          Description,
+          TotalDuration,
+          Warmup,
+          Cooldown,
+          IntervalsT,
+        }) => (
+          <WorkoutCard
+            name={Name}
+            description={Description}
+            totalDuration={TotalDuration}
+            warmupDuration={Warmup.Duration}
+            warmupPowerLow={Warmup.PowerLow}
+            warmupPowerHigh={Warmup.PowerHigh}
+            warmupCadence={Warmup.Cadence}
+            cooldownDuration={Cooldown.Duration}
+            cooldownPowerLow={Cooldown.PowerLow}
+            cooldownPowerHigh={Cooldown.PowerHigh}
+            cooldownCadence={Cooldown.Cadence}
+            repetitions={IntervalsT.Repeat}
+            onDuration={IntervalsT.OnDuration}
+            intervalCadence={IntervalsT.Cadence}
+            onPower={IntervalsT.OnPower}
+            offDuration={IntervalsT.OffDuration}
+            intervalCadenceResting={IntervalsT.CadenceResting}
+            offPower={IntervalsT.OffPower}
+          />
+        )
+      )}
+    </AppLayout>
   )
 }
 
-export default App
+const AppLayout = styled.div`
+  display: grid;
+  padding: 2px;
+  gap: 10px;
+  overflow-y: scroll;
+`
--- src/components/WorkoutCard/WorkoutCard.js
+++ src/components/WorkoutCard/WorkoutCard.js
@@ -1,16 +1,49 @@
 import styled from 'styled-components/macro'
 
-export default function WorkoutCard() {
+export default function WorkoutCard({
+  name,
+  description,
+  totalDuration,
+  warmupDuration,
+  warmupPowerLow,
+  warmupPowerHigh,
+  warmupCadence,
+  cooldownDuration,
+  cooldownPowerLow,
+  cooldownPowerHigh,
+  cooldownCadence,
+  repetitions,
+  onDuration,
+  intervalCadence,
+  onPower,
+  offDuration,
+  intervalCadenceResting,
+  offPower,
+}) {
   return (
     <WorkoutCardGrid>
-      <WorkoutCardTitle>On FTP-Light</WorkoutCardTitle>
+      <WorkoutCardTitle>{name}</WorkoutCardTitle>
       <WorkoutWrapper>
-        Warmup <br />
-        <WarmUpSection>XXmin from XX to XXW </WarmUpSection>
-        Main <br />
-        <MainSection>XXmin from XX to XXW </MainSection>
-        Cooldown <br />
-        <CooldownSection>XXmin from XX to XXW </CooldownSection>
+        <WorkoutDescription>
+          Description: {description} <br />
+          <br />
+          Total duration: {totalDuration}
+          <br />
+        </WorkoutDescription>
+        <WarmUpSection>
+          {warmupDuration}min from {warmupPowerLow} to {warmupPowerHigh}W at{' '}
+          {warmupCadence}rpm
+        </WarmUpSection>
+        <MainSection>
+          {' '}
+          {repetitions}x {onDuration}min @ {intervalCadence}rpm, {onPower}W,{' '}
+          <br />
+          {offDuration}min @ {intervalCadenceResting}rpm, {offPower}W{' '}
+        </MainSection>
+        <CooldownSection>
+          {cooldownDuration}min from {cooldownPowerLow} to {cooldownPowerHigh}W
+          at {cooldownCadence}rpm
+        </CooldownSection>
       </WorkoutWrapper>
     </WorkoutCardGrid>
   )
@@ -18,34 +51,42 @@ export default function WorkoutCard() {
 
 const WorkoutCardGrid = styled.section`
   display: grid;
-  gap: 10px;
+  gap: 5px;
   padding: 20px;
-  border-radius: 4px;
-  background: lightgray;
+  border-radius: 5px;
+  background: #cad7f6;
   width: 100%;
 `
 
 const WorkoutCardTitle = styled.h3`
+  text-align: justify;
+`
+
+const WorkoutDescription = styled.div`
   text-align: left;
+  border: 2px solid lightgray;
+  padding: 5px;
 `
 
 const WorkoutWrapper = styled.section`
-  text-align: left;
   display: grid;
   gap: 10px;
 `
 
 const WarmUpSection = styled.div`
+  text-align: right;
   background: lightblue;
   padding: 5px;
 `
 
 const MainSection = styled.div`
+  text-align: right;
   background: royalblue;
   padding: 5px;
 `
 
 const CooldownSection = styled.div`
+  text-align: right;
   background: lightskyblue;
   padding: 5px;
 `
