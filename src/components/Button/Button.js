a07be681ae4159d1288b0bb8a0b0941c9c15428f
Berenice Pendzialek <berenice.pendzialek@gmail.com>
2021-03-11 16:24:39 +0100

    Add button component to workout card and toogle function

---
+++ src/components/Button/Button.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components/macro'
+
+export default styled.button`
+  border: 1px solid orange;
+  width: 30%;
+  padding: 8px;
+  text-decoration: none;
+  color: inherit;
+  text-align: center;
+  background: transparent;
+`
