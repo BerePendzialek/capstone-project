8aef7cb195ed47089e2a50db0bfe497cfbe61d85
Berenice Pendzialek <berenice.pendzialek@gmail.com>
2021-03-19 17:02:31 +0100

    Add select inputs and modify track list

--- src/components/MusicForm/MusicForm.js
+++ src/components/MusicForm/MusicForm.js
@@ -1,9 +1,36 @@
-import { data } from '../tracks.json'
+import styled from 'styled-components/macro'
+import Button from '../Button'
 
 export default function MusicForm() {
   return (
-    <div>
-      <p></p>
-    </div>
+    <Form>
+      <Label>
+        Energy
+        <SelectInput name="energy" id="energy-select">
+          <option value="relax">Chill</option>
+          <option value="hard">More Power</option>
+        </SelectInput>
+      </Label>
+
+      <Label>
+        Genre
+        <SelectInput name="genre" id="genre-select">
+          <option value="alternative-rock">Alternative Rock</option>
+          <option value="latin">Latin</option>
+        </SelectInput>
+      </Label>
+      <Button>Get my Playlist! </Button>
+    </Form>
   )
 }
+
+const Form = styled.form`
+  display: grid;
+`
+
+const SelectInput = styled.select`
+  border: 1px solid green;
+`
+const Label = styled.label`
+  background: gray;
+`
--- src/components/MusicPage/MusicPage.js
+++ src/components/MusicPage/MusicPage.js
@@ -1,25 +1,23 @@
 import styled from 'styled-components/macro'
-import {
-  ShowFormatWarmup,
-  ShowFormatIntervalsT,
-  ShowFormatCooldown,
-} from '../WorkoutCard/ShowFormatFromSections'
-import { selectColor, selectColorByInterval } from '../../services/selectColor'
+
+// import {
+//   ShowFormatWarmup,
+//   ShowFormatIntervalsT,
+//   ShowFormatCooldown,
+// } from '../WorkoutCard/ShowFormatFromSections'
+// import { selectColor, selectColorByInterval } from '../../services/selectColor'
 import Button from '../Button'
 import Icon from 'supercons'
 import { NavLink } from 'react-router-dom'
+import MusicForm from '../MusicForm'
 
-export default function MusicPage({ location }) {
-  const { name, warmup, intervalsT, cooldown } = location.workout
-
+export default function MusicPage() {
   return (
     <Grid>
       <Heading> Select your music:</Heading>
 
-      <Filter>
-        <p>Lets filter</p>
-      </Filter>
-
+      <MusicForm />
+      {/* 
       <Title>
         {name} <br />
       </Title>
@@ -38,7 +36,7 @@ export default function MusicPage({ location }) {
             <ShowFormatCooldown cooldown={cooldown} />
           </CooldownSection>
         </div>
-      </Wrapper>
+      </Wrapper> */}
 
       <BackButton as={NavLink} to={{ pathname: '/' }}>
         <Icon glyph="back" size={24} />
@@ -69,44 +67,40 @@ const Heading = styled.h2`
   display: inline-block;
 `
 
-const Title = styled.h3`
-  margin: 15px 5px 0 5px;
-  text-align: justify;
-  padding: 2px;
-`
+// const Title = styled.h3`
+//   margin: 15px 5px 0 5px;
+//   text-align: justify;
+//   padding: 2px;
+// `
 
-const Filter = styled.section`
-  display: grid;
-`
+// const Wrapper = styled.section`
+//   display: grid;
+//   gap: 2px;
+// `
 
-const Wrapper = styled.section`
-  display: grid;
-  gap: 2px;
-`
+// const WarmupSection = styled.section`
+//   text-align: right;
+//   background: ${props => props.background};
+//   padding: 5px;
+//   border-radius: 5px;
+// `
 
-const WarmupSection = styled.section`
-  text-align: right;
-  background: ${props => props.background};
-  padding: 5px;
-  border-radius: 5px;
-`
+// const IntervalsTSection = styled.section`
+//   text-align: right;
+//   background: ${props => props.background};
+//   padding: 5px;
+//   margin-top: 2px;
+//   border-radius: 5px;
+// `
 
-const IntervalsTSection = styled.section`
-  text-align: right;
-  background: ${props => props.background};
-  padding: 5px;
-  margin-top: 2px;
-  border-radius: 5px;
-`
-
-const CooldownSection = styled.section`
-  text-align: right;
-  background: ${props => props.background};
-  padding: 5px;
-  margin-top: 2px;
-  margin-bottom: 25px;
-  border-radius: 5px;
-`
+// const CooldownSection = styled.section`
+//   text-align: right;
+//   background: ${props => props.background};
+//   padding: 5px;
+//   margin-top: 2px;
+//   margin-bottom: 25px;
+//   border-radius: 5px;
+// `
 
 const BackButton = styled(Button)`
   width: 50%;
